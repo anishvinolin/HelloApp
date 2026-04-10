c7150a4a7372d51d3819ae26c73c529106ee56a0
anishvinolin <anishvinolin15@gmail.com>
2026-04-10 10:43:25 +0530

    Added UC6 code

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -1,26 +1,20 @@
 package org.example;
 
 public class HelloApp {
-
     public static void main(String[] args) {
 
-        // If no arguments are provided
         if (args.length == 0) {
             System.out.println("Hello, World!");
             return;
         }
 
-        // Build comma-separated names using StringBuilder
-        StringBuilder names = new StringBuilder();
+        StringBuilder nameBuilder = new StringBuilder();
 
         for (String name : args) {
-            if (names.length() > 0) {
-                names.append(", ");
-            }
-            names.append(name);
+            nameBuilder.append(name).append(", ");
         }
+        String names = nameBuilder.substring(0, nameBuilder.length() - 2);
 
-        // Print greeting
         System.out.println("Hello, " + names + "!");
     }
 }
