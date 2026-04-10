33aa10c998a4a49639af93f3b46cfa53016153e9
anishvinolin <anishvinolin15@gmail.com>
2026-04-10 09:13:36 +0530

    Added UC5 code

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -1,14 +1,26 @@
 package org.example;
 
 public class HelloApp {
+
     public static void main(String[] args) {
-        //UC4
-        String names;
-        if (args.length > 0) {
-            names = String.join(", ", args);
-        } else {
-            names = "World";
+
+        // If no arguments are provided
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
         }
+
+        // Build comma-separated names using StringBuilder
+        StringBuilder names = new StringBuilder();
+
+        for (String name : args) {
+            if (names.length() > 0) {
+                names.append(", ");
+            }
+            names.append(name);
+        }
+
+        // Print greeting
         System.out.println("Hello, " + names + "!");
     }
 }
